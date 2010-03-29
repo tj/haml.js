cbe3dd6045f564e75ce7f402fc92c086b5a4901e
visionmedia <tj@vision-media.ca>
2010-03-29 10:04:41 -0700

    Fixed dedent buf when \n is not followed by whitespace. Closes #8

--- examples/example.js
+++ examples/example.js
@@ -2,4 +2,13 @@
 var sys = require('sys'),
     fs = require('fs'),
     haml = require('../lib/haml')
-sys.puts(haml.render(fs.readFileSync('examples/page.haml'), { locals: { items: ['foo', 'bar', 'baz'] }, filename: 'examples/page.haml' }))
+    
+var options = {
+  locals: {
+    title: 'Welcome',
+    body: 'wahoo',
+    usersOnline: 15
+  }
+}
+
+sys.puts(haml.render(fs.readFileSync('examples/page.haml'), options))
